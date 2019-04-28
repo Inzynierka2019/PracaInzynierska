e5aa52450d78a257bfd4fab74d10a12b17a3ec12
Jacek Ardanowski <jacekard@gmail.com>
2019-04-28 10:07:08 +0200

    Renamed solution files. Suppressed SDK warnings

--- Visualization/Web/Controllers/HomeController.cs
+++ Visualization/Web/Controllers/HomeController.cs
@@ -4,9 +4,9 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Inzynierka.Models;
+using Web.Models;
 
-namespace Inzynierka.Controllers
+namespace Web.Controllers
 {
     public class HomeController : Controller
     {
