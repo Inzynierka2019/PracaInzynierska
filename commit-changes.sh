c6a9f616a194044c1ea0338e8e3a6b296bbd01fa
Jacek Ardanowski <jacekard@gmail.com>
2019-05-01 20:07:42 +0200

    Dodanie git_tutorial.txt

--- commit-changes.sh
+++ commit-changes.sh
@@ -1,5 +1,9 @@
+# Dodajemy wszystkie zmiany
 git add .
+# Commitujemy wszystkie zmiany. Wpisujemy opis commita w edytorze domyślnym.
 git commit
+# Zaciagane sa wszystkie zmiany z branch deva. W przypadku merge-conflictów dobrym programem Visual Studio Code.
 git pull origin dev
+# Pushujemy
 git push
 
