d55b85e31990b12244d1cde5ff96a2bb241cdf67
multicast96 <kamil.dakus@gmail.com>
2019-05-10 21:51:43 +0200

    Adding Unity template and SimulationLibs solution with SimulationCore project

---
+++ Simulation/SimulationLibs/SimulationCore/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace SimulationCore
+{
+    public class Class1
+    {
+        public string Test()
+        {
+            return "It works !!!";
+        }
+    }
+}
---
+++ Simulation/SimulationUnity/Assets/Scripts/UnityIntegrationTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SimulationCore;
+
+public class UnityIntegrationTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(new Class1().Test());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
