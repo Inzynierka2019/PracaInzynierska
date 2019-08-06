d452088881d1e5851fd70a6d87756c886dbc2264
Jacek Ardanowski <jacekard@gmail.com>
2019-08-06 19:49:47 +0200

    Added json configuration to web project
    Signed-off-by: Jacek Ardanowski <jacekard@gmail.com>

---
+++ Visualization/Web.Logic/Configuration/IUnityConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.Logic.Configuration
+{
+    public interface IUnityConfiguration
+    {
+        string UnityExe { get; set; }
+
+        string UnityAppExe { get; set; }
+
+        string ProjectPath { get; set; }
+
+        string BuildSimulationBatch { get; set; }
+
+        string RunSimulationBatch { get; set; }
+    }
+}
---
+++ Visualization/Web.Logic/Configuration/UnityConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.Logic.Configuration
+{
+    public class UnityConfiguration : IUnityConfiguration
+    {
+        public string UnityExe { get; set; }
+
+        public string UnityAppExe { get; set; }
+
+        public string ProjectPath { get; set; }
+
+        public string BuildSimulationBatch { get; set; }
+
+        public string RunSimulationBatch { get; set; }
+    }
+}
