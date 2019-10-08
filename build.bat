0d00b0ff19736915654d219a5d501164fd0ceaa8
Jacek <jacekard@gmail.com>
2019-10-08 15:47:02 +0200

    created build.bat

--- build.bat
+++ build.bat
@@ -1,1 +1,9 @@
-dotnet publish --project Visualization/Web.Api/Web.Api.csproj -c Release -o ./Output
+@echo off
+set projectDirectory=%cd%
+set unityPath="C:\Progra~1\Unity\Hub\Editor\2019.1.14f1\Editor\Unity.exe"
+
+call dotnet publish Visualization\Web -c Release -o %projectDirectory%\..\Output
+call %unityPath% -quit -batchmode -logFile ..\buildWindows.log -projectPath %projectDirectory%\Simulation\App -executeMethod SimulationBuilder.BuildWindows
+call %unityPath% -quit -batchmode -logFile ..\Output\Simulation\buildWebGL.log -projectPath %projectDirectory%\Simulation\App -executeMethod SimulationBuilder.BuildWebGL
+call robocopy %projectDirectory%\Simulation\App\out\Windows %projectDirectory%\..\Output\Simulation\Windows /E /XO
+call robocopy %projectDirectory%\Simulation\App\out\WebGL %projectDirectory%\..\Output\Simulation\WebGL /E /XO
